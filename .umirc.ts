747bae70a88d524861650bd75dffecd43609ee9f
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-04-08 18:52:57 +0800

    部署配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'design-doc-dumi'
+
 export default defineConfig({
-  title: 'HTML-To-MasterGo',
+  title: 'design-doc-dumi',
   mode: 'site',
   // more config: https://d.umijs.org/config
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
